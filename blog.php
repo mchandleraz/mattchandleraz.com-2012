92c58ec017de5c63513d6d9253241f4a8c6af860
Matt Chandler <mchandler@tannernet.net>
2013-05-30 08:25:35 -0700

    Built blog.php page, added dynamic body ID tags via php, added social media links to footer (need styling)

---
+++ blog.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+
+	<!--
+	//
+	// designed & developed in the 
+	// browser, by matt chandler.
+	//
+	-->	
+
+<html>
+	
+	<head>
+
+		<meta charset="utf-8">
+		<title>mattchandleraz.com</title>
+		<meta name="description" content="Phoenix-based web developer Matt Chandler. Specializing in front end web development and Wordpress as a CMS.">
+		<meta name="author" content="Matt Chandler">
+		<meta name="keywords" content="web developer, web development, web designer, phoenix, arizona, tempe, mesa, scottsdale">
+		
+		<!-- mobile specific -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+		<link rel="stylesheet" type="text/css" href="lib/css/style.css">
+		
+		<!-- html5 for older browsrers -->
+		<!--[if lt IE 9]>
+	        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	    <![endif]-->
+	    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+	    <script src="lib/js/mcaz.js"></script>
+
+	</head>
+
+	<body id="<?php echo basename(__FILE__, '.php'); ?>">
+		<div id="content">
+			<header id="header" class="group">
+				<nav>
+					<ul>
+						<li class="homeLink"><a href="#top">Home</a></li>
+						<li class="aboutLink"><a href="#about">About</a></li>
+						<li class="contactLink"><a href="#contact">Contact</a></li>
+						<li class="portfolioLink"><a href="#portfolio">Portfolio</a></li>
+					</ul>
+				</nav>
+				<hgroup>
+					<h1 id="headline"><a href="index.php">mattchandleraz.com</a></h1>
+					<h2 id="tagline"><strong>Front-End Web Developer &amp; UX Evangelist</strong></h2>
+				</hgroup>
+			</header>
+
+			<section class="content">
+
+				<h1>Whoops...</h1>
+				<h5><em>Okay, fine. I haven't built my blog yet. Some day, I promise.</em></h5>
+
+			</section>
+			
+		</div><!-- end content -->
+		<footer class="group">
+			<ul class="footer-text-widget">
+				<li><a href="http://stackoverflow.com/users/1669930/mchandleraz">StackOverflow</a></li>
+				<li><a href="http://www.behance.net/MattChandler">Behance</a></li>
+				<li><a href="https://twitter.com/mchandleraz">Twitter</a></li>
+				<li><a href="https://github.com/mchandleraz">Github</a></li>
+				<li><a href="http://mattchandleraz.com/blog.php">Blog</a></li>
+			</ul>
+		</footer>
+	</body>
+
+</html>
--- index.php
+++ index.php
@@ -31,7 +31,7 @@
 
 	</head>
 
-	<body>
+	<body id="<?php echo basename(__FILE__, '.php'); ?>">
 		<div id="content">
 			<header id="header" class="group">
 				<nav>
@@ -126,6 +126,13 @@
 			
 		</div><!-- end content -->
 		<footer class="group">
+			<ul class="footer-text-widget">
+				<li><a href="http://stackoverflow.com/users/1669930/mchandleraz">StackOverflow</a></li>
+				<li><a href="http://www.behance.net/MattChandler">Behance</a></li>
+				<li><a href="https://twitter.com/mchandleraz">Twitter</a></li>
+				<li><a href="https://github.com/mchandleraz">Github</a></li>
+				<li><a href="http://mattchandleraz.com/blog.php">Blog</a></li>
+			</ul>
 		</footer>
 	</body>
 
